63e7102cda7115d8580d49426526cf7a08421114
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-10 14:54:17 -0700

    enhances setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
 from setuptools import setup, find_packages
 
+# Read the README content for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="codespace_utils",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
+    author="Alex Figueroa",
+    author_email="cybrvybe@gmail.com",
+    description="Codespace Utils is a versatile CLI tool designed to supercharge your developer workflow.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cybrvybe/codespace-utils",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         'click',
         'colorama'
     ],
+    python_requires=">=3.6",
     entry_points={
-    'console_scripts': [
-        'util=core.print_dir:util',
-    ],
-
-}
-
+        'console_scripts': [
+            'util=core.print_dir:util',
+        ],
+    }
 )
